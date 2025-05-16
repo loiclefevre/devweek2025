b39c81b10f1cece31ae3655fafe547194680fc1d
loic_lefevre <loic.lefevre@oracle.com>
2025-05-16 14:02:55 +0200

    .env

--- src/ORDS.js
+++ src/ORDS.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 function ORDS() {}
 
 ORDS.prototype.getSchema = async function() {
-  return await axios.get('https://G04882E973A17CF-DEVWEEK.adb.eu-paris-1.oraclecloudapps.com/ords/loic/schema_repository/products', {}, { headers: { "content-type": "application/json" } })
+  return await axios.get(process.env.ORDS_URL+'/loic/schema_repository/products', {}, { headers: { "content-type": "application/json" } })
     .then( res => res.data.schema )
     .catch((err) => { console.error(err); });
 }
 
 ORDS.prototype.insertNewProduct = async function(productJSON) {
-  return await axios.post('https://G04882E973A17CF-DEVWEEK.adb.eu-paris-1.oraclecloudapps.com/ords/loic/schema_repository/products', productJSON, { headers: { "content-type": "application/json" } })
+  return await axios.post(process.env.ORDS_URL+'/loic/schema_repository/products', productJSON, { headers: { "content-type": "application/json" } })
     .then( res => res )
     .catch((err) => { console.error(err); });
 }
