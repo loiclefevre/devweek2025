c9b499af29d65969662bcc35d933a10232d88a78
loic_lefevre <loic.lefevre@oracle.com>
2025-05-16 14:10:16 +0200

    react app env

--- src/ORDS.js
+++ src/ORDS.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 function ORDS() {}
 
 ORDS.prototype.getSchema = async function() {
-  return await axios.get(process.env.ORDS_URL+'/loic/schema_repository/products', {}, { headers: { "content-type": "application/json" } })
+  return await axios.get(process.env.REACT_APP_ORDS_URL+'/loic/schema_repository/products', {}, { headers: { "content-type": "application/json" } })
     .then( res => res.data.schema )
     .catch((err) => { console.error(err); });
 }
 
 ORDS.prototype.insertNewProduct = async function(productJSON) {
-  return await axios.post(process.env.ORDS_URL+'/loic/schema_repository/products', productJSON, { headers: { "content-type": "application/json" } })
+  return await axios.post(process.env.REACT_APP_ORDS_URL+'/loic/schema_repository/products', productJSON, { headers: { "content-type": "application/json" } })
     .then( res => res )
     .catch((err) => { console.error(err); });
 }
