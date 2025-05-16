171159decfbc20c04edb572208c8a98c804ed4c2
loic_lefevre <loic.lefevre@oracle.com>
2025-05-16 13:34:37 +0200

    content-type

--- src/ORDS.js
+++ src/ORDS.js
@@ -3,13 +3,13 @@ import axios from 'axios';
 function ORDS() {}
 
 ORDS.prototype.getSchema = async function() {
-  return await axios.get('https://G04882E973A17CF-DEVWEEK.adb.eu-paris-1.oraclecloudapps.com/ords/loic/schema_repository/products', {})
+  return await axios.get('https://G04882E973A17CF-DEVWEEK.adb.eu-paris-1.oraclecloudapps.com/ords/loic/schema_repository/products', {}, { headers: { "content-type": "application/json-www-form-urlencoded" } })
     .then( res => res.data.schema )
     .catch((err) => { console.error(err); });
 }
 
 ORDS.prototype.insertNewProduct = async function(productJSON) {
-  return await axios.post('https://G04882E973A17CF-DEVWEEK.adb.eu-paris-1.oraclecloudapps.com/ords/loic/schema_repository/products', productJSON)
+  return await axios.post('https://G04882E973A17CF-DEVWEEK.adb.eu-paris-1.oraclecloudapps.com/ords/loic/schema_repository/products', productJSON, { headers: { "content-type": "application/json-www-form-urlencoded" } })
     .then( res => res )
     .catch((err) => { console.error(err); });
 }
